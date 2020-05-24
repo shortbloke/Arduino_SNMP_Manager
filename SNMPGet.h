81ea8327c9b77d2a6527efe95a33ef38c53bd457
Martin Rowan <martin@rowannet.co.uk>
2020-05-24 12:12:16 +0100

    Simplify building get-request

--- SNMPGet.h
+++ SNMPGet.h
@@ -1,9 +1,3 @@
-/*
-	MFR: SNMPGet.h
-	This file is almost an entire duplication of the SNMPTrap.h
-	This needs refactoring to avoid code duplication.
-*/
-
 #ifndef SNMPGet_h
 #define SNMPGet_h
 
@@ -20,8 +14,6 @@ public:
 	short _version;
 	const char* _community;
 	IPAddress agentIP;
-	//OIDType* getOID;
-	//TimestampCallback* uptimeCallback;
 	short requestID;
 	short errorID = 0;
 	short errorIndex = 0;
@@ -40,15 +32,7 @@ public:
 		_udp = udp;
 	}
 
-// MFR: Remove commented out code? This is present in traps functions
-/*	void setUptimeCallback(TimestampCallback* uptime) {
-		uptimeCallback = uptime;
-	}
-*/	
-
     void addOIDPointer(ValueCallback* callback);
-
-
     ValueCallbacks* callbacks = new ValueCallbacks();
     ValueCallbacks* callbacksCursor = callbacks;
 
@@ -89,72 +73,25 @@ public:
 };
 
 bool SNMPGet::build() {
+	// Build packet for making GetRequest
 	if (packet) { packet = 0; }
 	packet = new ComplexType(STRUCTURE);
 	packet->addValueToList(new IntegerType((int)_version));
 	packet->addValueToList(new OctetType((char*)_community));
 	ComplexType* getPDU;
 	getPDU = new ComplexType(GetRequestPDU);
-	
 	getPDU->addValueToList(new IntegerType(requestID));
 	getPDU->addValueToList(new IntegerType(errorID));
 	getPDU->addValueToList(new IntegerType(errorIndex));
 	ComplexType* varBindList = new ComplexType(STRUCTURE);
-
-	// MFR: removed commented out code?
-/*	getPDU->addValueToList(new TimestampType(*(uptimeCallback->value)));
-	getPDU->addValueToList(new OIDType(getOID->_value));
-	getPDU->addValueToList(new NetworkAddress(agentIP));
-*/
-
-	//getPDU->addValueToList(new TimestampType(*(uptimeCallback->value)));
 	
-
 	callbacksCursor = callbacks;
 	if (callbacksCursor->value) {
 		while (true) {
 			ComplexType* varBind = new ComplexType(STRUCTURE);
 			varBind->addValueToList(new OIDType(callbacksCursor->value->OID));
-			BER_CONTAINER* value;
-			switch (callbacksCursor->value->type) {
-				case INTEGER:
-					{
-						// MFR: Trap code uses callback which is commented out here?
-						value = new IntegerType(0);//*((IntegerCallback*)callbacksCursor->value)->value);
-						// value = new IntegerType(*((IntegerCallback*)callbacksCursor->value)->value);
-					}
-				break;
-				case TIMESTAMP:
-					{
-						value = new TimestampType(*((TimestampCallback*)callbacksCursor->value)->value);
-					}
-				break;
-				case STRING:
-					{
-						value = new OctetType(*((StringCallback*)callbacksCursor->value)->value);
-					}
-				break;
-				case COUNTER32:
-					{
-						value = new IntegerType(0);
-						// value = new Counter32(0);
-						// value = new Counter32(*((Counter32Callback*)callbacksCursor->value)->value);
-					}
-				break;
-				// case COUNTER64:
-				// 	{
-				// 		// value = new Counter64(0);
-				// 		// value = new Counter64(*((Counter64Callback*)callbacksCursor->value)->value);
-				// 	}
-				// break;
-				// case GUAGE32:
-				// 	{
-				// 		// value = new Guage(0);
-				// 		// value = new Guage(*((Guage32Callback*)callbacksCursor->value)->value);
-				// 	}
-				// break;
-			}
-			
+			// Value can be null for Request payload.
+			BER_CONTAINER* value = new NullType();
 			varBind->addValueToList(value);
 			varBindList->addValueToList(varBind);
 
@@ -165,7 +102,6 @@ bool SNMPGet::build() {
 			}
 		}
 	}
-
 	getPDU->addValueToList(varBindList);
 	packet->addValueToList(getPDU);
 	return true;
@@ -184,6 +120,5 @@ void SNMPGet::addOIDPointer(ValueCallback* callback) {
     } else 
         callbacks->value = callback;
 }
-
 
 #endif
